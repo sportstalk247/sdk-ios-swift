c13742342a292df58ee46d3b80f6694d3add81fb
angelo-lesano <ignus0001@gmail.com>
2020-07-23 23:49:47 +0800

    Fixed a bug where handle is not being parsed

--- SportsTalk247/SportsTalk247/Models/User/User.swift
+++ SportsTalk247/SportsTalk247/Models/User/User.swift
@@ -24,53 +24,14 @@ open class User: NSObject, Codable {
     public required convenience init(from decoder: Decoder) throws {
         self.init()
         let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.kind = try container.decodeIfPresent(String.self, forKey: .kind)
         self.userid = try container.decodeIfPresent(String.self, forKey: .userid)
+        self.handle = try container.decodeIfPresent(String.self, forKey: .handle)
         self.profileurl = try container.decodeIfPresent(String.self, forKey: .profileurl)
         self.banned = try container.decodeIfPresent(Bool.self, forKey: .banned)
         self.displayname = try container.decodeIfPresent(String.self, forKey: .displayname)
         self.handlelowercase = try container.decodeIfPresent(String.self, forKey: .handlelowercase)
         self.pictureurl = try container.decodeIfPresent(String.self, forKey: .pictureurl)
-        self.kind = try container.decodeIfPresent(String.self, forKey: .kind)
-    }
-    
-    public func convertIntoModel(response:[String:Any]) -> User {
-        if let userid = response["userid"] as? String {
-            self.userid = userid
-        }
-        
-        if let handle = response["handle"] as? String {
-            self.handle = handle
-        }
-        
-        if let profileurl = response["profileurl"] as? String {
-            self.profileurl = profileurl
-        }
-        
-        if let banned = response["banned"] as? Bool {
-            self.banned = banned
-        }
-        
-        if let displayname = response["displayname"] as? String {
-            self.displayname = displayname
-        }
-        
-        if let handlelowercase = response["handlelowercase"] as? String {
-            self.handlelowercase = handlelowercase
-        }
-        
-        if let pictureurl = response["pictureurl"] as? String {
-            self.pictureurl = pictureurl
-        }
-        
-        if let kind = response["kind"] as? String {
-            self.kind = kind
-        }
-
-        return self
-    }
-    
-    public func getUrlString() -> String {
-       return pictureurl ?? ""
     }
 }
 
